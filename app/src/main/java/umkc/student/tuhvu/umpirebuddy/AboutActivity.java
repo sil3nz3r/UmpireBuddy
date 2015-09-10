b04f00cebfae98722a9e83d1ca31949dbdf40552
sil3nz3r <tuvu23061992@gmail.com>
2015-09-10 08:42:02 -0500

    Reset + About button
    Add Reset button and About button on the Action Bar

---
+++ app/src/main/java/umkc/student/tuhvu/umpirebuddy/AboutActivity.java
@@ -0,0 +1,7 @@
+package umkc.student.tuhvu.umpirebuddy;
+
+/**
+ * Created by tuvu on 9/9/15.
+ */
+public class AboutActivity {
+}
--- app/src/main/java/umkc/student/tuhvu/umpirebuddy/MainActivity.java
+++ app/src/main/java/umkc/student/tuhvu/umpirebuddy/MainActivity.java
@@ -32,25 +32,34 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mBallButton.setOnClickListener(this);
     }
 
-    private void updateCount(int id) {
+    private void updateCount(int countId) {
 
         TextView mStrikeCount_tv = (TextView)findViewById(R.id.Strike_count);
         TextView mBallCount_tv = (TextView)findViewById(R.id.Ball_count);
 
-        switch (id) {
-            case R.id.Strike_count:
+        switch (countId) {
+            case R.id.Strike_count: // case of strike
+                // set the strike count
                 mStrikeCount_tv.setText(Integer.toString(mStrikeCount));
+
+                // set the ball count to 0 if the strike count is also 0
                 if (mStrikeCount == 0) {
                     mBallCount_tv.setText(Integer.toString(0));
                 }
                 break;
-            case R.id.Ball_count:
+            case R.id.Ball_count: // case of ball
+                // set the ball count
                 mBallCount_tv.setText(Integer.toString(mBallCount));
+
+                // set the strike count to 0 if the ball count is also 0
                 if (mBallCount == 0) {
                     mStrikeCount_tv.setText(Integer.toString(0));
                 }
                 break;
-            default:
+            default: // exception case is currently resetting the count
+                mStrikeCount_tv.setText(Integer.toString(0));
+                mBallCount_tv.setText(Integer.toString(0));
+                break;
         }
     }
 
@@ -60,13 +69,36 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.Strike_button:
                 mStrikeCount++;
                 if (mStrikeCount == 3) {
-                    AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
-                    dlgAlert.setMessage("Out!");
-                    dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            mStrikeCount = 0;
+                    final AlertDialog dlgAlert = new AlertDialog.Builder(this)
+                            .setMessage(R.string.Strike_out_text)
+                            .setPositiveButton(android.R.string.ok, null)
+                            .setCancelable(false)
+                            .create();
+
+                    dlgAlert.setOnShowListener(new DialogInterface.OnShowListener() {
+
+                        @Override
+                        public void onShow(DialogInterface dialog) {
+                            Button btn = dlgAlert.getButton(AlertDialog.BUTTON_POSITIVE);
+                            btn.setOnClickListener(new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View view) {
+                                    dlgAlert.dismiss();
+                                }
+                            });
                         }
                     });
+
+                    mStrikeCount = 0;
+
+
+//                    AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+//                    dlgAlert.setMessage("Out!");
+//                    dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int id) {
+//                        }
+//                    });
                     dlgAlert.show();
                 }
                 updateCount(R.id.Strike_count);
@@ -79,10 +111,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     dlgAlert.setMessage("Walk!");
                     dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-                            mBallCount = 0;
                         }
                     });
                     dlgAlert.show();
+                    mBallCount = 0;
                 }
                 updateCount(R.id.Ball_count);
                 break;
@@ -102,11 +134,22 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.reset:
+                mStrikeCount = 0;
+                mBallCount = 0;
+                updateCount(-1);
+                return true;
+            case R.id.about:
+                return true;
+            default:
         }
 
         return super.onOptionsItemSelected(item);
